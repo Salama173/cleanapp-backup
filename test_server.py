8de538fb9acb6925560144dc1579e5a86c60b4cf
Salama173a <mady217345@gmail.com>
2025-08-14 03:31:37 -0400

    fix login template

--- test_server.py
+++ test_server.py
@@ -39,7 +39,7 @@ def index():
     return render_template("index.html")     
 
 @app.route('/login')
-def login(username):
+def login():
     return render_template("login.html")
     
 @app.route('/collect', methods=['POST'])
